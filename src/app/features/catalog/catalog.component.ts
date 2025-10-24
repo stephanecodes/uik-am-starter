18a7e4da75b0554e9ccf3e9016b2f0c766eeb443
Mathis Peingnez <mathispeingnez@MacBook-Pro-de-Mathis.local>
2025-10-24 12:46:58 +0200

    First implementation of the catalog products

--- src/app/features/catalog/catalog.component.ts
+++ src/app/features/catalog/catalog.component.ts
@@ -1,21 +1,46 @@
-import {Component, HostBinding} from '@angular/core';
-import {UikAmModule} from "@visiativ/uik-am";
-import {MatButtonModule} from "@angular/material/button";
-import {MatCardModule} from "@angular/material/card";
-import {PageLayoutComponent} from '../../layout/page-layout/page-layout.component';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { UikAmModule } from "@visiativ/uik-am";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
+import { PageLayoutComponent } from '../../layout/page-layout/page-layout.component';
 
 @Component({
   selector: 'app-catalog',
+  standalone: true,
   imports: [
+    CommonModule,
     UikAmModule,
+    MatIconModule,
     MatButtonModule,
     MatCardModule,
     PageLayoutComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatButtonToggleModule
   ],
   templateUrl: './catalog.component.html',
-  // styles are defined in src/theme/components/app/_catalog.scss
+  styleUrls: ['./catalog.component.scss']
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
   @HostBinding('class') class = 'app-catalog';
+
+  products: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('http://localhost:3000/products')
+      .subscribe(data => {
+        this.products = data;
+      });
+  }
 }
