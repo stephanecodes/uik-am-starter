0335d9532d7a7d0e065e39ba2b92dfabf1dced92
Mathis Peingnez <mathispeingnez@MacBook-Pro-de-Mathis.local>
2025-10-24 15:50:15 +0200

    Pagination and filter for products catalog

--- src/app/features/catalog/catalog.component.ts
+++ src/app/features/catalog/catalog.component.ts
@@ -1,13 +1,14 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+
 import { UikAmModule } from "@visiativ/uik-am";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from '@angular/material/icon';
-import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatPaginatorModule, PageEvent } from "@angular/material/paginator";
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 import { PageLayoutComponent } from '../../layout/page-layout/page-layout.component';
@@ -33,14 +34,43 @@ import { PageLayoutComponent } from '../../layout/page-layout/page-layout.compon
 export class CatalogComponent implements OnInit {
   @HostBinding('class') class = 'app-catalog';
 
-  products: any[] = [];
+  allProducts: any[] = [];   // Tous les produits
+  products: any[] = [];      // Produits filtrés
+  pagedProducts: any[] = [];
+
+  pageSize = 10;
+  currentPage = 0;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.http.get<any[]>('http://localhost:3000/products')
       .subscribe(data => {
-        this.products = data;
+        this.allProducts = data.slice(0, 12);
+        this.products = [...this.allProducts];
+        this.updatePage();
       });
+  }
+
+  filterProducts(event: Event) {
+    const query = (event.target as HTMLInputElement).value.toLowerCase();
+
+    this.products = this.allProducts.filter(p =>
+      p.name.toLowerCase().includes(query)
+    );
+
+    this.currentPage = 0;
+    this.updatePage();
+  }
+
+  onPageChange(event: PageEvent) {
+    this.currentPage = event.pageIndex;
+    this.updatePage();
+  }
+
+  updatePage() {
+    const start = this.currentPage * this.pageSize;
+    const end = start + this.pageSize;
+    this.pagedProducts = this.products.slice(start, end);
   }
 }
